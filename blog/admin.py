d1e27d1df97c63d496e8b13a59907febafa749ff
davewatters <89341297+davewatters@users.noreply.github.com>
2022-03-26 16:04:53 +0000

    add models to admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Book, Meetup
 
-# Register your models here.
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    
+    list_display = ('title', 'author', 'year_published', 'members_rating', 'has_been_read')
+    prepopulated_fields = {'slug': ('title',)}
+    # list_filter = ('has_been_read')
+
+
+@admin.register(Meetup)
+class MeetupAdmin(admin.ModelAdmin):
+    
+    list_display = ('meetup_title', 'meetup_date', 'book1')
+    prepopulated_fields = {'slug': ('title',)}
