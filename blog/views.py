ff803e2d5f05cd89af615e2363c6a94f0885f71f
davewatters <89341297+davewatters@users.noreply.github.com>
2022-04-03 20:21:59 +0100

    add ability to create a new comment on a meetup

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Meetup, Comments, Book
+from .forms import CommentForm
 
 
 class MeetupList(generic.ListView):
     model = Meetup
+
+
+class BookList(generic.ListView):
+    model = Book
+    ordering = ['title']
 
 
 class MeetupDetail(View):
@@ -18,10 +24,31 @@ class MeetupDetail(View):
             {
                 'meetup': meetup,
                 'comments': comments,
+                'comment_form': CommentForm()
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        meetup = Meetup.objects.get(slug=slug)
+        comments = Comments.objects.filter(meetup=meetup.id).order_by('created_on')
+        
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment_form.instance.user = request.user
+            new_comment = comment_form.save(commit=False)
+            new_comment.meetup = meetup
+            new_comment.save()
+        else:
+            comment_form = CommentForm()
+
+        return render (
+            request,
+            'blog/meetup_detail.html',
+            {
+                'meetup': meetup,
+                'comments': comments,
+                'comment_form': comment_form,
             },
         )
 
 
-class BookList(generic.ListView):
-    model = Book
-    ordering = ['title']
