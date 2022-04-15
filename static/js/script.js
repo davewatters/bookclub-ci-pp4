be625556f0762c145e5c4114d938343d755a7afa
davewatters <89341297+davewatters@users.noreply.github.com>
2022-04-15 14:13:52 +0100

    add template blocks to fix navbar active link all pages

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,15 @@
 "use strict";
 
-// Dismiss system messages alert
+/**
+ * Dismiss system messages alert
+ */
 setTimeout(function () {
   let messages = document.getElementById('msg');
   let alert = new bootstrap.Alert(messages);
   alert.close();
 }, 2500);
+
+
+/**
+ * Event Listeners
+ */
