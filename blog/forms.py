c4b24d280cbe0fbac8fa922f598302ddc9c64aa6
davewatters <89341297+davewatters@users.noreply.github.com>
2022-04-15 15:35:25 +0100

    futher styling of meetup_detail, add star rating for book

--- blog/forms.py
+++ blog/forms.py
@@ -3,21 +3,24 @@ from .models import Book, Comments, Meetup
 
 
 class CommentForm(forms.ModelForm):
-    
+    '''
+    '''
     class Meta:
         model = Comments
         fields = ('body',)
 
 
 class MeetupForm(forms.ModelForm):
-    
+    '''
+    '''
     class Meta:
         model = Meetup
         fields = '__all__'
 
 
 class BookForm(forms.ModelForm):
-    
+    '''
+    '''
     class Meta:
         model = Book
         fields = '__all__'
