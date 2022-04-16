e10fbf2ceffbab7eaedb9d2300cf4b71dbdd00cd
davewatters <89341297+davewatters@users.noreply.github.com>
2022-04-16 13:14:59 +0100

    fix: Fix bug in book_form, correct enctype for image upload

--- blog/forms.py
+++ blog/forms.py
@@ -2,8 +2,18 @@ from django import forms
 from .models import Book, Comments, Meetup
 
 
+class BookForm(forms.ModelForm):
+    '''
+    Create a Book form. Uses all of the fields.
+    '''
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+
 class CommentForm(forms.ModelForm):
     '''
+    Create a member's Comment form. Only body field needs to be entered.
     '''
     class Meta:
         model = Comments
@@ -12,15 +22,8 @@ class CommentForm(forms.ModelForm):
 
 class MeetupForm(forms.ModelForm):
     '''
+    Create a Meetup form. Uses all of the fields.
     '''
     class Meta:
         model = Meetup
-        fields = '__all__'
-
-
-class BookForm(forms.ModelForm):
-    '''
-    '''
-    class Meta:
-        model = Book
         fields = '__all__'
